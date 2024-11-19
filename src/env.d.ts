d0f14b90645f131ead1870e348abdb4046cb0ec7
Jake Sheehan <jake.sheehan.dev@gmail.com>
2024-11-19 10:31:55 -0500

    WIP: Tue 19 Nov 2024 10:31:35 AM EST

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,6 +6,11 @@ interface ImportMetaEnv {
     readonly CONTENTFUL_PREVIEW_TOKEN: string;
     readonly CALL_TO_ACTION_ENTRY_ID: string;
     readonly SERVICE_DECK_ENTRY_ID: string;
-
+    readonly GUARANTEE_SECTION_ENTRY_ID: string;
+    readonly ABOUT_US_ENTRY_ID: string;
+    readonly CONTACT_US_ENTRY_ID: string;
+    readonly NEW_HOMES_SLIDESHOW_ENTRY_ID: string;
+    readonly ADDITIONS_SLIDESHOW_ENTRY_ID: string;
+    readonly REMODELS_SLIDESHOW_ENTRY_ID: string;
 }
 
